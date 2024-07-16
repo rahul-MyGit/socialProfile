9479ca47a3170fe5bbd4b9f48a77b97c87c7291b
Rahul <rahul.mymail1@gmail.com>
2024-07-17 00:10:48 +0530

    Add: firebase

---
+++ src/contexts/authContext/index.tsx
@@ -0,0 +1,52 @@
+import React ,{ useState, createContext, useEffect, useContext } from "react";
+import { auth } from "../../firebase/firebase";
+import { onAuthStateChanged, User } from "firebase/auth";
+
+interface AuthContextType {
+    currentUser: User | null;
+    userLoggedIn: boolean;
+    loading: boolean;
+}
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+type AuthProviderProps = {
+    children: React.ReactNode;
+};
+
+export function useAuth(){
+    return useContext(AuthContext);
+}
+
+export function AuthProvider({children} : AuthProviderProps){
+    const [currentUser, setCurrentUser] = useState<User | null>(null);
+    const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (user)=>{
+            if(user){
+                setCurrentUser({...user});
+                setUserLoggedIn(true);
+            }else{
+                setCurrentUser(null);
+                setUserLoggedIn(false);
+            }
+            setLoading(false)
+        })
+
+        return unsubscribe;
+    }, [])
+
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading,
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+
+}
